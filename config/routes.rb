cbac21fcf6a63c3809c226e9b057f52a4258e491
Brad Neilson <bneilson42@ymail.com>
2014-11-03 20:06:06 +0000

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Bloccit::Application.routes.draw do
 
   devise_for :users
   resources :users, only: [:update, :show, :index]
-  resources :topics do
-    resources :posts, except: [:index] do
-      resources :comments, only: [:create, :destroy]
+
+     resources :topics do
+     resources :posts, except: [:index]
+   end
+
+   resources :posts, only: [] do
+     resources :comments, only: [:create, :destroy]
+   end
   end
 
   get 'about' => 'welcome#about'
