7711df7b192eb27b1eef5457325fac1e0b33a5a4
Brad Neilson <bneilson42@ymail.com>
2014-11-03 19:55:54 +0000

    Destroy update

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+
+  respond_to :html, :js
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.post = @post
+    @new_comment = Comment.new
+
+    authorize @comment
+    end
+       def destroy
+     @topic = Topic.find(params[:topic_id])
+     @post = @topic.posts.find(params[:post_id])
+     @comment = @post.comments.find(params[:id])
+
+     authorize @comment
+     if @comment.destroy
+       flash[:notice] = "Comment was removed."
+       redirect_to [@topic, @post]
+     else
+       flash[:error] = "Comment couldn't be deleted. Try again."
+       redirect_to [@topic, @post]
+     end
+   end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Bloccit::Application.routes.draw do
   devise_for :users
   resources :users, only: [:update, :show, :index]
   resources :topics do
-    resources :posts, except: [:index], controller: 'topics/posts'
+    resources :posts, except: [:index] do
+      resources :comments, only: [:create, :destroy]
   end
 
   get 'about' => 'welcome#about'
